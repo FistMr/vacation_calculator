b0f63788b12a6453e27b12e0d08c52b5c44676d1
Pavel Puchkov <pavel.pu4ckow@yandex.ru>
2025-04-17 17:17:45 +0300

    написана основная логика МС

--- src/main/java/com/puchkov/vacation_calculator/dto/VacationResponseDto.java
+++ src/main/java/com/puchkov/vacation_calculator/dto/VacationResponseDto.java
@@ -1,6 +1,5 @@
 package com.puchkov.vacation_calculator.dto;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
--- src/main/java/com/puchkov/vacation_calculator/service/HolidayService.java
+++ src/main/java/com/puchkov/vacation_calculator/service/HolidayService.java
@@ -7,7 +7,6 @@ import org.springframework.web.client.RestTemplate;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
